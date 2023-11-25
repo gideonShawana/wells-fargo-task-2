475d5e73788548b02ffe5c3ca0784cfe3fcdfc2d
Gideon Oladipupo Shawana <135270937+gideonShawana@users.noreply.github.com>
2023-11-24 22:54:02 -0500

    adding portfolio.java to update repo

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,2 +1,54 @@
-package com.wellsfargo.counselor.entity;public class Portfolio {
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_id")
+    private Long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Client clientId;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Long portfolioId, Client clientId, Date creationDate) {
+        this.portfolioId = portfolioId;
+        this.clientId = clientId;
+        this.creationDate = creationDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(Long portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public Client getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Client clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
